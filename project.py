405eee92a0fcdac614a3f9d0599a400cf8ef82e5
garymorrissey <36278683+garymorrissey@users.noreply.github.com>
2018-04-24 23:31:13 +0100

    Update project.py

--- project.py
+++ project.py
@@ -10,13 +10,25 @@ with open ("Data/iris.csv") as f: # Link the csv file
 # Calculating the mean from each column
 # Petal Length
 import numpy
-data = numpy.genfromtxt('Data/iris.csv', delimiter=',')
+data = numpy.genfromtxt('Data/iris.csv', delimiter=',') # Used genfromtxt to load the data from a text file
 first_column = data[:,0] # Generating an array for column 1
 meanfirst_column = numpy.mean(data[:,0]) 
-print('The mean petal length of the three species in the dataset is', meanfirst_column) # Print the average of column 1 - Petal Length
+print('The mean petal length of the three species in the dataset in cm is', meanfirst_column) # Print the average of column 1 - Petal Length
 
 # Petal Width
-data = numpy.genfromtxt('Data/iris.csv', delimiter=',')
-second_column = first_column = data[:,1] # Generating an array for column 2
+data = numpy.genfromtxt('Data/iris.csv', delimiter=',') # Repeated genfromtxt
+second_column = data[:,1] # Generating an array for column 2
 meansecond_column = numpy.mean(data[:,1])
-print('The mean petal width of the three species in the dataset is', meansecond_column) # Print the average of column 1 - Petal Width
+print('The mean petal width of the three species in the dataset in cm is', meansecond_column) # Print the average of column 2 - Petal Width
+
+# Sepal Length
+data = numpy.genfromtxt('Data/iris.csv', delimiter=',') # Repeated genfromtxt
+third_column = data[:,2] # Generating an array for column 3
+meanthird_column = numpy.mean(data[:,2])
+print('The mean sepal length of the three species in the dataset in cm is', meanthird_column) # Print the average of column 3 - Sepal Length
+
+# Sepal Width
+data = numpy.genfromtxt('Data/iris.csv', delimiter=',') # Repeated genfromtxt
+fourth_column = data[:,3] # Generating an array for column 4
+meanfourth_column = numpy.mean(data[:,3])
+print('The mean sepal width of the three species in the dataset in cm is', meanfourth_column) # Print the average of column 4 - Sepal Width
